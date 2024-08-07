746252aa28a812646f3a560773f34dfaca0897c0
Yury <yury.timoschuk@biletum.com>
2024-08-07 17:39:46 +0300

    linear progress ability in Await component

--- src/Await.svelte
+++ src/Await.svelte
@@ -1,8 +1,10 @@
 <script>
-    import { ProgressCircular } from "material-components";
+    import ProgressCircular from "material-components/src/components/ProgressCircular/ProgressCircular.svelte";
+    import ProgressLinear from "material-components/src/components/ProgressLinear/ProgressLinear.svelte";
     import {fade} from 'svelte/transition';
     import Error from "./Error.svelte";
 
+    export let linear = false;
     export let promise;
     let progress = false;
     $:  {
@@ -18,7 +20,11 @@
 {#await promise}
     {#if progress}
         <div class="progress" transition:fade={{duration: 200}}>
-            <ProgressCircular size={70} width={3} indeterminate color="green" />
+            {#if linear}
+                <ProgressLinear indeterminate color="green" />
+            {:else}
+                <ProgressCircular size={70} width={3} indeterminate color="green" />
+            {/if}
         </div>
     {/if}
 {:then data}
