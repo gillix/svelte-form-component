3d437feb6d1fc081e4c86c6f94d0bff9fd627c88
Yury <yury.timoschuk@biletum.com>
2025-02-14 18:15:06 +0200

    request backend with credentials in workflow

--- src/workflow/request.js
+++ src/workflow/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function request(url, data = {}, callback = null) {
     try {
-        return (await axios.post(url, data)).data;
+        return (await axios.post(url, data, {withCredentials: true})).data;
     } catch (error) {
         throw new Error(error.response.data.error);
     }
